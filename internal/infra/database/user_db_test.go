9e460dd51ba0bd29109348f7178a1372baa72c4d
Kayky Marinho <kayky.marinho18@gmail.com>
2024-10-15 23:10:13 -0300

    tests and product db

--- internal/infra/database/user_db_test.go
+++ internal/infra/database/user_db_test.go
@@ -29,3 +29,24 @@ func TestCreateUser(t *testing.T) {
 	assert.Equal(t, user.Email, userFound.Email)
 	assert.NotNil(t, userFound.Password)
 }
+
+func TestFindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	assert.Nil(t, err)
+
+	db.AutoMigrate(&entity.User{})
+
+	user, _ := entity.NewUser("test", "test@", "123123")
+
+	userDB := NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail("test@")
+
+	assert.Nil(t, err)
+	assert.Equal(t, user.Id, userFound.Id)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNil(t, userFound.Password)
+}
