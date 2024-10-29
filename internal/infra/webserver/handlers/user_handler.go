097a3b4f73b04d094c28f130b16644e138fb9efa
Kayky Marinho <kayky.marinho18@gmail.com>
2024-10-29 16:53:59 -0300

    feat: create GetJWT

--- internal/infra/webserver/handlers/user_handler.go
+++ internal/infra/webserver/handlers/user_handler.go
@@ -3,18 +3,56 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/Kayky18/API_FULLCYCLE/internal/dto"
 	"github.com/Kayky18/API_FULLCYCLE/internal/entity"
 	"github.com/Kayky18/API_FULLCYCLE/internal/infra/database"
+	"github.com/go-chi/jwtauth"
 )
 
 type UserHandler struct {
-	UserDB database.UserInterface
+	UserDB        database.UserInterface
+	Jwt           *jwtauth.JWTAuth
+	JwtExperiesIn int
 }
 
-func NewUserHandler(u database.UserInterface) *UserHandler {
-	return &UserHandler{UserDB: u}
+func NewUserHandler(u database.UserInterface, jwt *jwtauth.JWTAuth, jwtExperiesIn int) *UserHandler {
+	return &UserHandler{
+		UserDB:        u,
+		Jwt:           jwt,
+		JwtExperiesIn: jwtExperiesIn,
+	}
+}
+
+func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
+	var user dto.GetJWTInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	u, err := h.UserDB.FindByEmail(user.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+	}
+	if !u.ValidatePassoword(user.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	_, tokenString, _ := h.Jwt.Encode(map[string]interface{}{
+		"sub": u.ID.String(),
+		"exp": time.Now().Add(time.Second * time.Duration(h.JwtExperiesIn)).Unix(),
+	})
+	acessToken := struct {
+		AccessToken string `json:"access_token"`
+	}{
+		AccessToken: tokenString,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(acessToken)
+
 }
 
 func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
